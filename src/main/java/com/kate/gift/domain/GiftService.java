9bab7394474aee77b577bd981f6c744e5e1330f0
KATEKEITH <46472768+KATEKEITH@users.noreply.github.com>
2023-11-27 11:10:57 +0900

    refactor #4 remove service interface

--- src/main/java/com/kate/gift/domain/GiftService.java
+++ src/main/java/com/kate/gift/domain/GiftService.java
@@ -1,7 +1,49 @@
 package com.kate.gift.domain;
 
-public interface GiftService {
+import com.kate.gift.domain.gift.order.OrderApiCaller;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-    GiftInfo registerOrder(GiftCommand.Register request);
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class GiftService {
+
+//    private final GiftReader giftReader;
+
+    private final GiftStore giftStore;
+
+    private final OrderApiCaller orderApiCaller;
+
+    private final GiftToOrderMapper giftToOrderMapper;
+
+    /**
+     * 선물하기 주문을 등록한다
+     * 해당 주문을 주문 서비스에 등록하기 위해 API 를 호출하고
+     * 등록된 주문의 식별키와 선물 관련 정보를 반영하여 Gift 도메인을 저장한다
+     *
+     * @param request
+     * @return
+     */
+    @Transactional
+    public GiftInfo registerOrder(GiftCommand.Register request) {
+        log.info("GiftServiceImpl request = {}", request);
+
+        var orderCommand = giftToOrderMapper.of(request);
+
+        log.info("GiftServiceImpl orderCommand = {}", orderCommand);
+
+        var orderToken = orderApiCaller.registerGiftOrder(orderCommand);
+
+        log.info("GiftServiceImpl orderToken = {}", orderToken);
+
+        var initGift = request.toEntity(orderToken);
+
+        var gift = giftStore.store(initGift);
+
+        return new GiftInfo(gift);
+    }
 
 }
