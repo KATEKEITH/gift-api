175bfd112223b7298b83a6a0bad992de8a89f47c
KATEKEITH <46472768+KATEKEITH@users.noreply.github.com>
2023-12-01 09:23:01 +0900

    add application.yml local,dev,prod 으로 분리 및 환경 변수 파일 추가

--- src/main/java/com/kate/gift/GiftApplication.java
+++ src/main/java/com/kate/gift/GiftApplication.java
@@ -2,10 +2,11 @@ package com.kate.gift;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/secret.properties")
 public class GiftApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GiftApplication.class, args);
 	}
